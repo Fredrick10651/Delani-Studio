4d36aaf7d3d7baf41cb83b07c64bc00f6d988ea9
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-06-20 10:59:46 +0300

    Add jQuery codes on toggling between image and paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
-$(document).ready(function() {
-    $(".process.one").click(function() {
-      $(".paint-showing").toggle();
-      $(".paint-hidden").toggle();
+
+$(document).ready(function () {
+    $(".design-1").click(function () {
+        $(".first").toggle();
+        $(".des").toggle();
     });
-  });
+});
+$(document).ready(function () {
+    $(".design-2").click(function () {
+        $(".second").toggle();
+        $(".dev").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $(".design-3").click(function () {
+        $(".third").toggle();
+        $(".pro").toggle();
+    });
+});
+
+
+
+$(document).ready(function(){
+    $(".pic").hover(function(){
+        $(this).find("div").toggleClass("pic-description_show")
+    })
+})
