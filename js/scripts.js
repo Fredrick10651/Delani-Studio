9ee2557a4620c5863d9374a0a3dfd4c426cacaed
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-06-21 13:07:18 +0300

    Add alert message in js

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,8 @@ $(document).ready(function(){
     })
 })
 
+$(document).ready(function () {
+    $(".btn").click(function () {
+        alert('We have received your message. Thank you for reaching out to us.');
+    })
+});
